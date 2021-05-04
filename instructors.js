9e59f20417bd52ef3537fd43e3473f179b42384b
Denis Batista <denis.si2011@gmail.com>
2021-05-04 18:51:06 -0300

    Trabalhando com dados

---
+++ instructors.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const data = require("./data.json")
+
+//create
+exports.post = function (req, res) {
+    //req.query
+
+
+    //requ.body
+    // { "avatar_url": "http://google.com", "name": "Denis Jesus", "birth": "2021-05-03", "gender": "M", "services": "Teste" }
+
+
+    //["avatar_url","name","birth","gender","services"]
+    const keys = Object.keys(req.body)
+
+
+    for (key of keys) {
+        //requ.body.key == ""
+        if (req.body[key] == "")
+            return res.send("Please, fill all fields!")
+    }
+
+    req.body.birth = Date.parse(req.body.birth);
+
+    req.body.create_at = Date.now();
+
+
+    data.instructors.push(req.body)
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
+        if (err) return res.send("Write file error!");
+
+        return res.redirect("instructors")
+
+    });
+
+    // return res.send(req.body);
+
+}
+
--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express = require('express'); /*Importar os arquivos*/
 const routes = express.Router();
+const instructors = require('./instructors')
 
 routes.get('/', function (req, res) {
     return res.redirect("/instructors")
@@ -14,27 +15,7 @@ routes.get('/instructors/create', function (req, res) {
     return res.render('instructors/create')
 });
 
-routes.post("/instructors", function (req, res) {
-    //req.query
-
-
-    //requ.body
-    // { "avatar_url": "http://google.com", "name": "Denis Jesus", "birth": "2021-05-03", "gender": "M", "services": "Teste" }
-
-
-    //["avatar_url","name","birth","gender","services"]
-    const keys = Object.keys(req.body)
-
-
-    for (key of keys) {
-        //requ.body.key == ""
-        if (req.body[key] == "")
-            return res.send("Please, fill all fields!")
-    }
-
-    return res.send(req.body);
-
-});
+routes.post("/instructors", instructors.post);
 
 routes.get('/members', function (req, res) {
     return res.render("members")
