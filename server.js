86140ca3e55a7403ae973d4838cde16b56a512c9
Denis Batista <denis.si2011@gmail.com>
2021-05-19 18:46:31 -0300

    MethoOverride - Sobrecrita

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
 const routes = require("./routes")
+const methodOverride = require("method-override")
 
 
 /*Chamar a função pra dentro do servidor*/
@@ -9,13 +10,13 @@ const server = express();
 
 server.use(express.urlencoded({ extended: true }))
 
-
-
-
 /*Usando arquivos staticos*/
 server.use(express.static('public'));
 
+server.use(methodOverride("_method"))
+
 server.use(routes);
+
 
 server.set("view engine", "njk");
 
